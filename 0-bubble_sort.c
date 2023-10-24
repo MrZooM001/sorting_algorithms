9963c037968c34e3c39174d1e47e9668d5baa811
Hazem Magdy <hhazem0010@gmail.com>
2023-10-24 11:33:08 -0700

    sorting algorithms init project

---
+++ 0-bubble_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm.
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+size_t i, arr_len;
+int is_bubble;
+
+if (array == NULL || size < 2)
+{
+return;
+}
+
+arr_len = size;
+is_bubble = 0;
+while (is_bubble == 0)
+{
+is_bubble = 1;
+for (i = 0; i < arr_len; i++)
+{
+if (array[i] > array[i + 1])
+{
+int tmp;
+
+tmp = array[i];
+array[i] = array[i + 1];
+array[i + 1] = tmp;
+print_array(array, size);
+is_bubble = 0;
+}
+}
+arr_len--;
+}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Tasks of ***"0x1B. C - Sorting algorithms & Big O"*** project <br />
+Scope <br />
+***C Language*** <br />
+
